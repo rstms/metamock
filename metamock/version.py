1a3b7cd37e159501611dbf0ab9cc978ffe461df9
Matt Krueger <mkrueger@rstms.net>
2022-05-31 21:18:41 -0500

    Bump version: 0.2.4 → 0.2.5

--- metamock/version.py
+++ metamock/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-05-31 21:10:35-05:00"
+__timestamp__ = "2022-05-31 21:18:40-05:00"
